195c540c08cc44547c145268b0dde94d0b3bacdb
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2018-01-19 00:39:44 +0800

    changed the login page

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -1,23 +1,33 @@
 <template>
 	<div class="container">
+		<div class="right-side">
+       <h1>DPS Electronic Voting System</h1>
+			 <p>A project of the DPS Comelec</p>
+			 <el-tag>v0.9.1</el-tag>
+		</div>
 		<div class="login">
-			<h1 class="login__main-heading">Log In to the EVS</h1>
-			<form @submit.prevent="login" class="login__form">
-				<div class="login__input-wrapper">
-					<label class="login__label" for="usernameInput">Username</label>
-					<input type="text" id="usernameInput" placeholder="Username" v-model="username" class="login__input">
-				</div>
-				<div class="login__input-wrapper">
-					<label for="passwordInput" class="login__label">Password</label>
-					<input type="password" id="passwordInput" placeholder="Password" v-model="password" class="login__input">
-				</div>
-				<button type="submit" class="login__submit">Submit</button>
-			</form>
+			<svg viewBox="0 0 1779 274" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" class="svgbaby"><path d="M29.395 240C-110.804 215.219 290.284 0 396.638 0c106.355 0 166.664 240 270.885 240S913.924 0 1021.964 0c108.039 0 205.297 240 293.794 240 88.498 0 164.081-240 237.193-240 73.112 0 298.878 224.657 201.478 240-253.926 40-1498.74 40-1725.03 0z" fill="#dcdfe6"/><path d="M252.359 272.12c-166.883-8.354 341.188-240 468.32-240 121.659 0 190.248 240 294.469 240s232.924-240 330.856-240c97.932 0 368.271 247.805 256.734 240-182.311-12.756-1199.19 7.569-1350.38 0z" fill="#ebeef5"/></svg>
+	 	  <svg viewBox="0 0 1779 274" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" class="svgbabys"><path d="M29.395 240C-110.804 215.219 290.284 0 396.638 0c106.355 0 166.664 240 270.885 240S913.924 0 1021.964 0c108.039 0 205.297 240 293.794 240 88.498 0 164.081-240 237.193-240 73.112 0 298.878 224.657 201.478 240-253.926 40-1498.74 40-1725.03 0z" fill="#dcdfe6"/><path d="M252.359 272.12c-166.883-8.354 341.188-240 468.32-240 121.659 0 190.248 240 294.469 240s232.924-240 330.856-240c97.932 0 368.271 247.805 256.734 240-182.311-12.756-1199.19 7.569-1350.38 0z" fill="#ebeef5"/></svg>
+			<div >
+				<h1 class="login__main-heading">Log In to the EVS</h1>
+				<form @submit.prevent="login" class="login__form">
+					<el-form label-position="top" label-width="100px">
+						<el-form-item label="Username">
+							<el-input v-model="username"></el-input>
+						</el-form-item>
+						<el-form-item label="Password">
+							<el-input v-model="password" type="password"></el-input>
+						</el-form-item>
+					</el-form>
+					<el-button type="primary" @click="login">Login</el-button>
+				</form>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -60,7 +70,7 @@ export default {
   mounted() {
     //this.$store.commit(this.$const.CLEAR_SELECTED_PARTY)
     this.getParties()
-  }
+	}
 }
 </script>
 
@@ -88,54 +98,46 @@ html {
 
 .container {
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 }
 
 .login {
-  border: 1px solid black;
-  padding: 16px 40px;
-  border-radius: 2px;
-  padding-bottom: 24px;
+  display: flex;
+  align-items:center;
+  padding-top: 30px;
+	position:relative;
+	padding-left:60px;
 }
 
-.login__main-heading {
-  font-weight: 300;
-  color: #333;
-  margin-bottom: 8px;
+.svgbaby {
+	position:absolute;
+	bottom:-30px;
+	left:50%;
+	right:0;
+	transform: translateX(-50%);
 }
 
-.login__form {
-  display: flex;
-  flex-direction: column;
+.svgbabys{
+	position:absolute;
+	left:50%;
+	right:0;
+	top:-30px;
+	transform: rotate(180deg) translateX(50%);
 }
-.login__input-wrapper {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 16px;
+
+
+
+.right-side {
+	background-color: #F2F6FC;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	flex-direction:column;
 }
-.login__label {
-  font-size: 20px;
-  font-weight: 700;
-}
-.login__input {
-  padding: 4px 8px;
-  padding-left: 4px;
-  font-size: 16px;
-  color: #555;
-  border-radius: 1px;
-  border: 1px solid #888;
-}
-.login__submit {
-  border: 0;
-  background-color: var(--blue);
-  color: white;
-  padding: 8px 24px;
-  border-radius: 2px;
-  text-transform: lowercase;
-  font-weight: 700;
-  font-size: 12px;
-  font-variant: small-caps;
+
+.right-side > :nth-child(2) {
+	margin-bottom:8px;
 }
 </style>
+
--- src/renderer/components/Voting.vue
+++ src/renderer/components/Voting.vue
@@ -6,63 +6,62 @@
 			<el-step title="Step 3" description="Exit Quietly"></el-step>
 		</el-steps>
 		<form @submit.prevent="submit">
-		
-             <!-- Pres -->
-      <el-card class="box-card" v-for="party in party_group.parties" :key="party.party_name">
-        <div slot="header">
-           <h2>{{toTitleCase(party.party_name)}} </h2>
-        </div>
-        <div>
+			<!-- Pres -->
+			<el-card class="box-card" v-for="party in party_group.parties" :key="party.party_name">
+				<div slot="header">
+					<h2>{{toTitleCase(party.party_name)}} </h2>
+				</div>
+				<div>
+					<el-radio v-model="president" :label="party.president.name" v-if="party.president.name" size="medium">
+						{{toTitleCase(party.president.name)}}
+					</el-radio>
+
+					<el-radio v-else disabled size="medium">President</el-radio>
+					<!-- VP -->
+					<el-radio v-model="vice_president" :label="party.vice_president.name" v-if="party.vice_president.name" size="medium">
+						{{toTitleCase(party.vice_president.name)}}
+					</el-radio>
+
+					<el-radio v-else disabled size="medium">Vice President</el-radio>
+					<!-- SEC -->
+					<el-radio v-model="secretary" :label="party.secretary.name" v-if="party.secretary.name" size="medium">
+						{{toTitleCase(party.secretary.name)}}
+					</el-radio>
+
+					<el-radio v-else disabled size="medium">Secretary</el-radio>
+
+					<!-- Treasurer -->
+					<el-radio v-model="treasurer" :label="party.treasurer.name" v-if="party.treasurer.name" size="medium">
+						{{toTitleCase(party.treasurer.name)}}
+					</el-radio>
+					<el-radio v-else disabled size="medium">Treasurer</el-radio>
+					<!-- LSP -->
+					<el-radio v-model="lsp" :label="party.lsp.name" v-if="party.lsp.name" size="medium">
+						{{toTitleCase(party.lsp.name)}}
+					</el-radio>
+					<el-radio v-else disabled size="medium">
+						Lower School Representative
+					</el-radio>
+				</div>
+			</el-card>
 
 
-        <el-radio v-model="president" :label="party.president.name" v-if="party.president.name" size="medium">
-          {{toTitleCase(party.president.name)}}
-        </el-radio>
 
-        <el-radio v-else disabled size="medium">President</el-radio>
-      <!-- VP -->
-        <el-radio v-model="vice_president" :label="party.vice_president.name" v-if="party.vice_president.name" size="medium">
-           {{toTitleCase(party.vice_president.name)}}
-        </el-radio>
-
-        <el-radio v-else disabled  size="medium">Vice President</el-radio>
-     <!-- SEC -->
-        <el-radio v-model="secretary" :label="party.secretary.name" v-if="party.secretary.name" size="medium">
-           {{toTitleCase(party.secretary.name)}}
-        </el-radio>
-
-        <el-radio v-else disabled  size="medium">Secretary</el-radio>
-
-     <!-- Treasurer -->
-        <el-radio v-model="treasurer" :label="party.treasurer.name" v-if="party.treasurer.name" size="medium">
-           {{toTitleCase(party.treasurer.name)}}
-        </el-radio>
-        <el-radio v-else disabled size="medium">Treasurer</el-radio>
-             <!-- LSP -->
-        <el-radio v-model="lsp" :label="party.lsp.name" v-if="party.lsp.name" size="medium">
-           {{toTitleCase(party.lsp.name)}}
-        </el-radio>
-        <el-radio v-else disabled  size="medium">Lower School Representative</el-radio>
-    </div>
-      </el-card>
-	
-
-		
-<el-card class="box-card">
-      <div slot="header">
-           <h2>Abstain</h2>
-        </div>
-        <div>
-        <el-radio v-model="president" label="abstain">Abstain</el-radio>
-        <el-radio v-model="vice_president" label="abstain">Abstain</el-radio>
-        <el-radio v-model="secretary" label="abstain">Abstain</el-radio>
-        <el-radio v-model="treasurer" label="abstain">Abstain</el-radio>
-        <el-radio v-model="lsp" label="abstain">Abstain</el-radio>
-        </div>
-</el-card>
+			<el-card class="box-card">
+				<div slot="header">
+					<h2>Abstain</h2>
+				</div>
+				<div>
+					<el-radio v-model="president" label="abstain">Abstain</el-radio>
+					<el-radio v-model="vice_president" label="abstain">Abstain</el-radio>
+					<el-radio v-model="secretary" label="abstain">Abstain</el-radio>
+					<el-radio v-model="treasurer" label="abstain">Abstain</el-radio>
+					<el-radio v-model="lsp" label="abstain">Abstain</el-radio>
+				</div>
+			</el-card>
 
 
-			<el-button type="primary" @click="submit">Vote</el-button>
+			<el-button type="primary button" @click="submit">Vote</el-button>
 		</form>
 	</div>
 </template>
@@ -86,9 +85,11 @@ export default {
     back() {
       this.$router.push('/')
     },
-    toTitleCase(str){
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-},
+    toTitleCase(str) {
+      return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+      })
+    },
     submit() {
       const {
         president,
@@ -134,26 +135,26 @@ export default {
 </script>
 
 
-<style scoped>
+<style>
 .box-card {
   width: 80%;
   margin: 40px auto;
 }
-button {
- text-transform: uppercase;
- font-variant: small-caps;
- letter-spacing: 0.1px;
-  font-size: 40px;
 
+.button {
+  text-transform: uppercase;
+  font-variant: small-caps;
+  letter-spacing: 0.1px;
+  font-size: 40px;
   display: block;
   margin: 40px auto;
 }
 
 .el-radio__label {
-  font-size:18px;
-} 
+  font-size: 18px;
+}
 
 h2 {
-  font-size:30px;
+  font-size: 30px;
 }
 </style>
