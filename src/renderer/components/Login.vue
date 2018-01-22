4bd21e5c3144258a4c5121d3f79d53e1f9ea8d79
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2018-01-22 12:31:22 +0800

    bumped login version number

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -3,7 +3,7 @@
 		<div class="right-side">
        <h1 id="h1ofright">DPS Electronic Voting System</h1>
 			 <p>A project of the DPS Comelec</p>
-			 <el-tag>v0.9.1</el-tag>
+			 <el-tag>v0.9.8</el-tag>
 		</div>
 		<div class="login">
 			<svg viewBox="0 0 1779 274" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" class="svgbaby"><path d="M29.395 240C-110.804 215.219 290.284 0 396.638 0c106.355 0 166.664 240 270.885 240S913.924 0 1021.964 0c108.039 0 205.297 240 293.794 240 88.498 0 164.081-240 237.193-240 73.112 0 298.878 224.657 201.478 240-253.926 40-1498.74 40-1725.03 0z" fill="#dcdfe6"/><path d="M252.359 272.12c-166.883-8.354 341.188-240 468.32-240 121.659 0 190.248 240 294.469 240s232.924-240 330.856-240c97.932 0 368.271 247.805 256.734 240-182.311-12.756-1199.19 7.569-1350.38 0z" fill="#ebeef5"/></svg>
